64a30ca72f1146698d1cd0bf31ff31f461125332
Conway Ying <yingkongwei@gmail.com>
2017-11-20 23:41:58 +1100

    Add styling

--- daily/forms.py
+++ daily/forms.py
@@ -1,7 +1,16 @@
 from django import forms
 
 class AreaForm(forms.Form):
-    your_name = forms.CharField(label='Your Area', max_length=20)
+    your_name = forms.CharField(
+        label='', 
+        max_length=20,
+        required=True,
+        widget=forms.TextInput(attrs={'class': 'special'})
+    )
 
 class TaskForm(forms.Form):
-    your_name = forms.CharField(label='Your Task', max_length=50, required=False)
+    your_name = forms.CharField(
+        label='', 
+        max_length=50, 
+        required=False
+    )
