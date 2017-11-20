b8e551730b36d6058f6dca2a48fe62f37fe3a072
Conway Ying <yingkongwei@gmail.com>
2017-11-20 13:38:48 +1100

    Refactor form handling

--- daily/forms.py
+++ daily/forms.py
@@ -4,4 +4,4 @@ class AreaForm(forms.Form):
     your_name = forms.CharField(label='Your Area', max_length=20)
 
 class TaskForm(forms.Form):
-    your_name = forms.CharField(label='Your Task', max_length=50)
+    your_name = forms.CharField(label='Your Task', max_length=50, required=False)
--- daily/views.py
+++ daily/views.py
@@ -4,17 +4,23 @@ from django.utils import timezone
 from django.utils.timezone import localtime
 from django.urls import reverse
 from django.http import HttpResponseRedirect
+from django.contrib import messages
 
 from .models import Area, Task, Interval
 from .forms import AreaForm, TaskForm
 
 def index(request):
     if request.method == 'POST':
-        form = AreaForm(request.POST)
-        if form.is_valid():
-            name = form.cleaned_data['your_name']
-            Area.add(name)
-            return redirect('daily:index')
+        if request.POST.get('add_area'):
+            form = AreaForm(request.POST)
+            if form.is_valid():
+                name = form.cleaned_data['your_name']
+                Area.add(name)
+        elif request.POST.get('add_task'):
+            add_task(request)
+        elif request.POST.get('start_interval') or request.POST.get('end_interval'):
+            add_interval(request)
+        return redirect('daily:index')
     else:
         today = localtime(timezone.now()).date()
         today_areas_list = Area.objects.filter(date__date=today)
@@ -31,27 +37,33 @@ def index(request):
 def add_task(request):
     if request.method == 'POST':
         form = TaskForm(request.POST)
-        area = Area.objects.get(pk=request.POST['choice'])
-        if form.is_valid():
-            name = form.cleaned_data['your_name']
-            task = Task.add(name, area, 0)
-            print(task)
-            return redirect('daily:index')
-    else:
-        return redirect('daily:index')
+        try:
+            area = Area.objects.get(pk=request.POST['area_choice'])
+            if not form.data['your_name']:
+                raise Exception()
+        except (KeyError, Area.DoesNotExist):
+            messages.error(request, 'You didn\'t select an Area to add a Task to')
+        except (Exception):
+            messages.error(request, 'You didn\'t enter a name for the Task')
+        else:
+            if form.is_valid():
+                name = form.cleaned_data['your_name']
+                task = Task.add(name, area, 0)
+    return redirect('daily:index')
     
 def add_interval(request):
     if request.method == 'POST':
-        if 'start' in request.POST:
-            task = Task.objects.get(pk=request.POST['choice'])
-            Interval.stop_active_interval(task)
-            Interval.add(task)
-        elif 'end' in request.POST:
-            task = Task.objects.get(pk=request.POST['choice'])
-            Interval.stop_active_interval(task)
-        return redirect('daily:index')
-    else:
-        return redirect('daily:index')
+        try:
+            task = Task.objects.get(pk=request.POST['task_choice'])
+        except:
+            messages.error(request, 'You didn\'t select a Task to add an Interval to')
+        else:
+            if 'start_interval' in request.POST:
+                Interval.stop_active_interval(task)
+                Interval.add(task)
+            elif 'end_interval' in request.POST:
+                Interval.stop_active_interval(task)
+    return redirect('daily:index')
 
 class IndexView(generic.ListView):
     template_name = 'daily/index.html'
