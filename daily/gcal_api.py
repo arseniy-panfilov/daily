bfe66019b9e2d48b9286b43a5df1622cb1eae275
Conway Ying <yingkongwei@gmail.com>
2017-11-21 13:03:11 +1100

    Add gcal API integration

---
+++ daily/gcal_api.py
@@ -0,0 +1,102 @@
+
+from __future__ import print_function
+import httplib2
+import os
+
+from django.conf import settings
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+import datetime
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/calendar-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Google Calendar API Python Quickstart'
+
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'calendar-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        print(client_secret_file_dir)
+        client_secret_file_dir = os.path.join(settings.BASE_DIR, settings.STATIC_URL, 'daily', 'static', CLIENT_SECRET_FILE)
+        flow = client.flow_from_clientsecrets(client_secret_file_dir, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def add_interval_as_event(task, interval):
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('calendar', 'v3', http=http)
+
+    event = {
+        'summary': task.name,
+        'start': {
+            'dateTime': interval.start.isoformat('T'),
+            'timeZone': interval.start.tzname(),
+        },
+        'end': {
+            'dateTime': interval.end.isoformat('T'),
+            'timeZone': interval.end.tzname(),
+        }
+    }
+    event = service.events().insert(calendarId='primary', body=event).execute()
+    print("Event created: %s" % (event.get('htmlLink')))
+
+def main():
+    """Shows basic usage of the Google Calendar API.
+
+    Creates a Google Calendar API service object and outputs a list of the next
+    10 events on the user's calendar.
+    """
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('calendar', 'v3', http=http)
+
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    eventsResult = service.events().list(
+        calendarId='primary', timeMin=now, maxResults=10, singleEvents=True,
+        orderBy='startTime').execute()
+    events = eventsResult.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'])
+
+if __name__ == '__main__':
+    main()
