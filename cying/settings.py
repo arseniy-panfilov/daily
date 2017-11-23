c7a0d090ee071c4ed982f8852d842e2d2cb786f7
Conway Ying <yingkongwei@gmail.com>
2017-11-23 17:00:08 +1100

    Add app.yaml and change settings

--- cying/settings.py
+++ cying/settings.py
@@ -142,4 +142,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'http://storage.googleapis.com/<your-gcs-bucket>/static/'
